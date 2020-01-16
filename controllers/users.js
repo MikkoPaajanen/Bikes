f76753f68b7ef6123bfd07167c65eaf6399ad416
MikkoPaajanen <paajasenmikko@gmail.com>
2020-01-16 13:44:45 +0200

    userschema made and posting users now possible

---
+++ controllers/users.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcryptjs')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async (req, res, next) => {
+  try {
+    const body = req.body
+    if (!body.password) {
+      return res.status(401).json({ error: 'password missing' })
+    }
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+    const user = new User({
+      username: body.username,
+      firstname: body.firstname,
+      lastname: body.lastname,
+      passwordHash
+    })
+
+    const savedUser = await user.save()
+    res.json(savedUser)
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+usersRouter.get('/', async (req, res, next) => {
+  try {
+    const users = await User
+      .find({}).populate('bikes', { id: 1})
+    res.json(users.map(u => u.toJSON()))
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+module.exports = usersRouter
---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  passwordHash: String,
+  bikes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Bike'
+    }
+  ]
+})
+
+userSchema.plugin(uniqueValidator)
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
